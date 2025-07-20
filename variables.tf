05cd9d03ce1daabc2181ec16796fd04532607124
Radu Dragan <157606156+radu-dragon@users.noreply.github.com>
2025-07-21 01:25:29 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "s3_bucket_name" {
   type = string
-  description = "S3 bucket unique name to be assigned"
+  description = "S3 bucket unique name to be assigned to"
   default = "radu-spin-s3"
 }
 
